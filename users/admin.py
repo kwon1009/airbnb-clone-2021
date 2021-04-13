5b2e7c95211a52dd2d90a40bcf56026af1d78d69
kwonhs <47519135+kwon1009@users.noreply.github.com>
2021-04-13 14:56:35 +0900

    create user (#3.4)

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+
+    """ Custom User Admin """
+
+    list_display = ("username", "gender", "language", "currency", "superhost")
+    list_filter = (
+        "language",
+        "currency",
+        "superhost",
+    )

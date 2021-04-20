c2d3e84aa25d70f00d1ac836645fd26a278b8066
kwonhs <47519135+kwon1009@users.noreply.github.com>
2021-04-20 12:58:07 +0900

    Filtering (#13.6)
    - price, guests, etc.
    - instant, superhost
    - amenities, facilities

--- rooms/views.py
+++ rooms/views.py
@@ -49,8 +49,8 @@ def search(request):
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
 
@@ -64,7 +64,7 @@ def search(request):
         "beds": beds,
         "baths": baths,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
     }
@@ -89,6 +89,35 @@ def search(request):
 
     if room_type != 0:
         filter_args["room_type__pk"] = room_type
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            filter_args["amenities__pk"] = int(s_amenity)
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            filter_args["facilities__pk"] = int(s_facility)
 
     rooms = models.Room.objects.filter(**filter_args)
 

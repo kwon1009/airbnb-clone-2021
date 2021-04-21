013bb54d35ccd64e307f97a683ed75a8114502b9
kwonhs <47519135+kwon1009@users.noreply.github.com>
2021-04-21 19:24:42 +0900

    Sending verify email (#16.3)

--- config/settings.py
+++ config/settings.py
@@ -149,3 +149,4 @@ EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "tester@sandbox6cadd77233144269a011ac01a69310f0.mailgun.org"

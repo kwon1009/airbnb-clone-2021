f16a382087d9a70546fd3dc1f0d572e538e61926
kwonhs <47519135+kwon1009@users.noreply.github.com>
2021-04-26 00:28:29 +0900

    Finishing room_card and pagination (#20.7)

--- rooms/views.py
+++ rooms/views.py
@@ -10,7 +10,7 @@ class HomeView(ListView):
     """ Home View Definition """
 
     model = models.Room
-    paginate_by = 10
+    paginate_by = 12
     ordering = "created"
     context_object_name = "rooms"
 

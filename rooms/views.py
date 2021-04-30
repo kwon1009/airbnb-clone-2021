8fef2a9e2e38db539a89abaaea927b62ece1ab94
kwonhs <47519135+kwon1009@users.noreply.github.com>
2021-04-30 22:59:58 +0900

    add photo (#23.6)

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.views.generic import ListView, DetailView, View, UpdateView
+from django.views.generic import ListView, DetailView, View, UpdateView, FormView
 from django.shortcuts import render, redirect, reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
@@ -212,3 +212,20 @@ class EditPhotoView(
     def get_success_url(self):
         room_pk = self.kwargs.get("room_pk")
         return reverse("rooms:photos", kwargs={"pk": room_pk})
+
+
+class AddPhotoView(
+    user_mixins.LoggedInOnlyView,
+    FormView,
+):
+
+    model = models.Photo
+    template_name = "rooms/photo_create.html"
+    fields = ("caption", "file")
+    form_class = forms.CreatePhotoForm
+
+    def form_valid(self, form):
+        pk = self.kwargs.get("pk")
+        form.save(pk)
+        messages.success(self.request, "Photo Upload")
+        return redirect(reverse("rooms:photos", kwargs={"pk": pk}))
